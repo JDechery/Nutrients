3b3744e919867061fe3ca727bc93f4d3cdf04164
JDechery <jdechery@gmail.com>
2017-10-09 21:09:05 -0500

    create nutrient and food tables
    table structure needs to be sorted out

--- create_nutrient_database.py
+++ create_nutrient_database.py
@@ -7,44 +7,43 @@ logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('nutrient_
 
 apikey = '0nDIJuT0aCiNbIiwxVIpAWRUKauc4EdLNQgSjUc1'
 
-report_url = 'https://api.nal.usda.gov/ndb/report'
-nitems = 900
+report_url = 'https://api.nal.usda.gov/ndb/reports'
+nitems = 1
 keeprunning = True
 
 dbfile = 'F:/Data/nutrients_database.sqlite'
 conn = sqlite3.connect(dbfile)
 c = conn.cursor()
 
-item_ids = c.execute("SELECT ids from items")
-
+item_ids = c.execute("SELECT id from items").fetchall()
 nitem = 1
-data = {'format' : 'json',
-        'type'   : 'b'
-        'ndbno'  : '',
-        'api_key': apikey}
-while keeprunning:
+data = [['format', 'json'], ['type', 'b'], ['ndbno', '0'], ['api_key', apikey]]
 
-    data['ndbno'] = item_ids[nitem]
-    nutrient_report = requests(report_url, params=data)
+while keeprunning:
+    ndbno = item_ids[nitem][0]
+    if len(str(ndbno))<5:
+        padsize = 5-len(str(ndbno))
+        pad = ['0' for _ in range(padsize)]
+        ndbno = ''.join(pad+list(str(ndbno)))
+
+    data[2][1] = ndbno
+    nutrient_report = requests.get(report_url, params=data)
     if nutrient_report.status_code != 200:
         logging.warn('request error: http code {code}'.format(code=item_list.status_code))
         keeprunning = False
     else:
         logging.info('item {i}'.format(i=nitem))
         json_data = nutrient_report.json()
-
+        json_data = json_data['report']['food']
+        foodname = json_data['name']
+        nutrient_data = []
+        for nut in json_data['nutrients']:
+            nutrient_data.append((int(nut['nutrient_id']), nut['name'], float(nut['value']), nut['unit']))
         #items = json_data['list']['item']
         #item_ids = [(int(it['id']), it['name']) for it in items]
         #item_ids = dict(item_ids)
         #nutrient_data = [()]
 
-        for data in nutrient_data
-            try:
-                c.execute('INSERT OR IGNORE INTO items VALUES (?, ?);', (key, val))
-            except Exception as e:
-                logging.error('database operation failed')
-                logging.error(str(e))
-                break
 
     conn.commit()
     nloop += 1
--- initialize_nutrient_table.py
+++ initialize_nutrient_table.py
@@ -1,4 +1,5 @@
 import sqlite3
+import requests
 
 dbfile = 'F:/Data/nutrients_database.sqlite'
 conn = sqlite3.connect(dbfile)
@@ -11,15 +12,25 @@ list_url = 'https://api.nal.usda.gov/ndb/list'
 data = (('format', 'json'), ('lt', 'n'), ('sort', 'n'), ('max', nitems), ('offset', 0), ('api_key', apikey))
 
 nutrient_response = requests.get(list_url, params=data)
+#print(nutrient_response.status_code)
+# %% create table for individual nutrient metadata
 nutrient_data = nutrient_response.json()
 nutrient_data = nutrient_data['list']['item']
 nutrient_ids = [(n['id'], n['name']) for n in nutrient_data]
+#uids = list(set([int(x[0]) for x in nutrient_ids]))
 
-# %% create tables with column for each nutrient
-query = 'CREATE TABLE nutrients ('
+creation_query = 'CREATE TABLE nutrients (id INT PRIMARY KEY, name TEXT)'
+c.execute(creation_query)
+c.executemany('INSERT INTO nutrients (id, name) VALUES (?, ?)', nutrient_ids)
+conn.commit()
+
+# %% create tables with column for each nutrient; column name is foreign key to nutrient table
+nutrient_fk = sorted([x[0] for x in nutrient_ids])
+query = 'CREATE TABLE foods ('
 col12 = 'id INT PRIMARY KEY, name TEXT, '
-col3plus = ', '.join(['{} TEXT'.format(n[1]) for n in nutrient_ids])
+col3plus = ', '.join(['n{} REAL'.format(n) for n in nutrient_fk])
 query = ''.join([query, col12, col3plus, ');'])
+print(query)
 c.execute(query)
 
 conn.commit()
